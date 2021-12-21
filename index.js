367915b11bcea5ec6543a4d251b44021ea33494a
aswinkumarbaskar <93180931+aswinkumarbaskar@users.noreply.github.com>
2021-12-20 22:29:58 -0800

    Add files via upload

---
+++ index.js
@@ -0,0 +1,26 @@
+const express=require("express");
+const device=require("express-device");
+const app=express();
+const router=express.Router();
+
+	app.use(device.capture());
+	app.use("/aswinkumar",express.static("./images"))
+router.get("/login",(req,res)=>{
+	if(req.device.type=="desktop")
+	{
+		res.sendFile("C:/Users/Suganya/AppData/Roaming/Sublime Text 3/Packages/User/node/express/aSAMPLES/index.html");
+	}
+	else
+	{
+		res.sendFile("C:/Users/Suganya/AppData/Roaming/Sublime Text 3/Packages/User/node/express/aSAMPLES/images/index.html");	
+	}
+});
+
+router.get("/logout",(req,res)=>{
+	res.json({"Message":"successfully logged out"})
+});
+
+app.use("/aswinkumar",router);
+app.listen(8080,()=>{
+console.log("server running....!");
+});
